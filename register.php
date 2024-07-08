2aa8b50266b61f780d0abd74aae895ebd2af05d4
chhotu <chhotu456787@gmail.com>
2024-07-08 22:15:00 +0530

    Change register form

--- register.php
+++ register.php
@@ -3,15 +3,29 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Registration form</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link src="style.css">
     </link>
+    <style>
+        .formstyle{
+            height: 30rem;
+            width: 30rem;
+        }
 
+        @media (max-width: 320px) { 
+            .formstyle{
+                height: 30rem;
+                width: 20rem;
+            }
+         }
+    </style>
+</head>
 <body>
-    <!-- <div class="conatiner"> -->
-        <h1>Registration form</h1>
+    <div class="container formstyle">
+        <h1 style="text-align:center">Registration Form</h1>
+        <br>
 <!-- 
         <fom action="function.php" name="registerform1" id="registerform1" method="POST">
 
@@ -27,18 +41,18 @@
         <form class="row g-3" action="function.php" name="registerform1" id="registerform1" method="POST">
             <div class="col-md-12">
                 <label for="input" class="form-label">Email</label>
-                <input type="email" class=" form-control username" name="username" id="username">
+                <input type="text" class=" form-control username" name="username" id="username">
             </div>
             <div class="col-md-12">
                 <label for="password" class="form-label">Password</label>
                 <input type="password" class="form-control pass" name="pass" id="pass">
             </div>     
-            <div class="col-12">
+            <div class="col-12" style="text-align:center">
                     <input type="hidden" name="registerform" id="registerform" value="submitted">
-                <button type="submit" class="btn btn-primary">Sign in</button>
+                <button style="height:3rem; width:7rem; font-size:1.5rem;" type="submit" class="btn btn-primary">Sign in</button>
             </div>
         </form>
-    <!-- </div> -->
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
@@ -47,3 +61,4 @@
         integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
         crossorigin="anonymous"></script>
 </body>
+</html>
---
+++ signin.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<link src= "style.css"> </link>
+<style>
+</style>
+<script>
+
+function submit1()
+{
+	document.getElementById('signin').submit();
+
+}
+
+</script>
+
+
+
+</head>
+<body>
+Login Page
+
+<form action="function.php" name="signin" id="signin" method="POST">
+
+<Input type="text" name="username" id="username">
+<br>
+<input type="password" name="pass" id="pass"  >
+<br>
+<input type="hidden" name="signinform" id="signinform" value="submitted">
+<br>
+<input type="submit">
+
+</form>
+
+</body>
